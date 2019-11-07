6f6c12a64091ed6bb70a1596d1b0782c6140c35b
rajat10538 <rajat.mittal@invenio-solutions.com>
2019-11-07 11:57:46 +0000

    commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"assignment/Formatter/Formatters/Reusables" 
+], function (Controller, Reusables) {
+	"use strict";
+
+	return Controller.extend("assignment.Formatter.controller.View1", {
+		test : Reusables,
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"assignment/Formatter/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
